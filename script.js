553041529a3dcf8e2e23af417764ae1fd8814f52
Monitoria Jurídica <66352542+gibagomes@users.noreply.github.com>
2025-12-26 23:54:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,52 +1,6 @@
-const input = document.getElementById("tarefa");
-const botaoAdicionar = document.getElementById("adicionar");
-const lista = document.getElementById("lista");
+const btn = document.getElementById("btn");
+const saida = document.getElementById("saida");
 
-let tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
-
-mostrarTarefas();
-
-botaoAdicionar.addEventListener("click", adicionarTarefa);
-
-function adicionarTarefa() {
-  const texto = input.value;
-
-  if (texto === "") {
-    alert("Digite uma tarefa!");
-    return;
-  }
-
-  tarefas.push(texto);
-  salvarTarefas();
-  mostrarTarefas();
-
-  input.value = "";
-}
-
-function mostrarTarefas() {
-  lista.innerHTML = "";
-
-  tarefas.forEach((tarefa, index) => {
-    const item = document.createElement("li");
-    item.textContent = tarefa;
-
-    const botaoRemover = document.createElement("button");
-    botaoRemover.textContent = "X";
-
-    botaoRemover.addEventListener("click", function () {
-      tarefas.splice(index, 1);
-      salvarTarefas();
-      mostrarTarefas();
-    });
-
-    item.appendChild(botaoRemover);
-    lista.appendChild(item);
-  });
-}
-
-function salvarTarefas() {
-  localStorage.setItem("tarefas", JSON.stringify(tarefas));
-}
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("service-worker.js");
-}
+btn.addEventListener("click", function () {
+  saida.textContent = "Funcionou! Você acabou de rodar JavaScript no celular.";
+});
